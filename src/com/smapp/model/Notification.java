0f3a3b4bf093dba8bef36654b2fd7cea603b1ad4
samarthbsb <samarth@bsb.in>
2015-04-12 23:44:01 +0530

    Notification test

---
+++ src/com/smapp/model/Notification.java
@@ -0,0 +1,25 @@
+package com.smapp.model;
+
+/**
+ * Created by samarth on 05/04/15.
+ */
+public class Notification {
+    String id;
+    String message;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
